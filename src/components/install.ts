4288a0138db5e43374c26a65379fa1e7d25a1f9b
kangjian <365@kangwuyi.com>
2025-03-24 16:31:06 +0800

    Update install.ts

--- src/components/install.ts
+++ src/components/install.ts
@@ -1,6 +1,6 @@
 import { type App } from 'vue'
 import Linking from './Linking.vue'
 Linking.install = function (app: App) {
-  app.component(Linking.name as string, Linking)
+  app.component('Linking', Linking)
 }
 export default Linking
